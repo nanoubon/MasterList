61270efd778a552559fbc4f511e0641ace361a24
arttaksin <solo_blackshadow@hotmail.com>
2017-09-29 14:01:56 +0700

    Add files via upload

---
+++ application/views/welcome_message.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en-gb" dir="ltr" class="uk-height-1-1">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login layout example - UIkit documentation</title>
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
+				 <link href="assets/css/uikit.min.css" rel="stylesheet">
+                <link href="assets/css/uikit.css" rel="stylesheet">
+        <script src="../vendor/jquery.js"></script>
+        <script src="assets/js/uikit.min.js"></script>
+    </head>
+
+    <body class="uk-height-1-1">
+			<center>
+        <div class="uk-vertical-align uk-text-center uk-height-1-1">
+            <div class="uk-vertical-align-middle" style="width: 250px;">
+
+                <img class="uk-margin-bottom" width="140" height="120" src="image/rotarylogo.jpg" alt="">
+
+                <form class="uk-panel uk-panel-box uk-form">
+                    <div class="uk-form-row">
+                        <input class="uk-width-1-1 uk-form-large" type="text" placeholder="Username">
+                    </div>
+                    <div class="uk-form-row">
+                        <input class="uk-width-1-1 uk-form-large" type="text" placeholder="Password">
+                    </div>
+                    <div class="uk-form-row">
+                        <a class="uk-width-1-1 uk-button uk-button-primary uk-button-large" href="#">Login</a>
+                    </div>
+                    <div class="uk-form-row uk-text-small">
+                        <label class="uk-float-left"><input type="checkbox"> Remember Me</label>
+                        <a class="uk-float-right uk-link uk-link-muted" href="#">Forgot Password?</a>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+			</center>
+    </body>
+
+</html>
